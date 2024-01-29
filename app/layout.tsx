5c61531e2ebe816879de1fdf00b4ec3f2c598863
Mert Meral <mertmr@gmail.com>
2024-01-29 07:09:59 +0000

    install speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,16 +22,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange><div className="flex">
-<Sidebar />
-<main className="flex-1 md:p-8 pt-2 p-8">
-<Navbar />
-{children}
-</main>
-</div>
-<Toaster />
-</ThemeProvider>
-</body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange><div className="flex">
+          <Sidebar />
+          <main className="flex-1 md:p-8 pt-2 p-8">
+            <Navbar />
+            {children}
+            <SpeedInsights />
+          </main>
+        </div>
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
